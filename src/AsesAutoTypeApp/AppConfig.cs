bdbed9207c51e8c09d18bb30a6813f7108089fe3
Tomislav Matas <tomislav.matas@deutschebahn.com>
2024-03-22 21:07:08 +0100

    main Version "24.123.0.0" Added initial prototyping into branch "main".

---
+++ src/AsesAutoTypeApp/AppConfig.cs
@@ -0,0 +1,24 @@
+﻿//
+// File: AppConfig.cs
+//
+// Summary:
+// Project specific configuration settings. 
+//
+// Revision History: 
+// 2024/03/22:TomislavMatas: Version "24.123.0.0"
+// * Initial version.
+//
+
+using ConfigApi = AsesAutoTypeLib.ConfigApi;
+
+namespace AsesAutoTypeApp
+{
+    /// <summary>
+    /// Project specific constants.
+    /// </summary>
+    public static class AppConfig
+    {
+
+    } // class
+
+} // namespace
