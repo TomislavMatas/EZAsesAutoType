c54f81ca6abceaaef80abc230c679335df11d4ed
Tomislav Matas <slove@matas.de>
2024-03-23 19:06:18 +0100

    main Version "24.123.0.0" Prototyping UI.

--- src/EZAsesAutoType/AppConfig.cs
+++ src/EZAsesAutoType/AppConfig.cs
@@ -9,15 +9,46 @@
 // * Initial version.
 //
 
-using ConfigApi = EZAsesAutoType.ConfigApi;
+using log4net;
 
 namespace EZAsesAutoType
 {
     /// <summary>
     /// Access app specific configuration settings from "App.config" file.
     /// </summary>
-    public static class AppConfig
+    internal static class AppConfig
     {
+        #region log4net
+        private static ILog? m_Log = null;
+        private static ILog Log
+        {
+            get
+            {
+                if (m_Log == null)
+                    m_Log = LogManager.GetLogger(typeof(AppConfig));
+                return m_Log;
+            }
+        }
+        #endregion
+
+        private static bool Initialze()
+        {
+            try
+            {
+                Log.Debug(Const.LogStart);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex);
+                return false;
+            }
+            finally
+            {
+                Log.Debug(Const.LogDone);
+            }
+
+        }
 
     } // class
 
