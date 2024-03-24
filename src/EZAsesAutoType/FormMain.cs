68e9896d962078a21df91114d0b74ff5ccd12e0c
Tomislav Matas <slove@matas.de>
2024-03-24 16:08:48 +0100

    main Version "24.123.0.0" Prototyping UI.

--- src/EZAsesAutoType/FormMain.cs
+++ src/EZAsesAutoType/FormMain.cs
@@ -6,7 +6,10 @@
 // * Initial version.
 //
 
+using EZAsesAutoType;
 using log4net;
+using Microsoft.VisualBasic.Logging;
+using System.Collections.Specialized;
 
 namespace EZAsesAutoType
 {
@@ -28,6 +31,7 @@ namespace EZAsesAutoType
         }
         #endregion
 
+        #region private propertiez
         private AppHandler? m_AppHandler = null;
         private AppHandler AppHandler
         {
@@ -42,17 +46,15 @@ namespace EZAsesAutoType
                 m_AppHandler = value;
             }
         }
-
-        internal AppHandler SeAppHandler(AppHandler appHandler)
+        private AppHandler GetAppHandler()
+        {
+            return this.AppHandler;
+        }
+        private AppHandler SetAppHandler(AppHandler appHandler)
         {
             AppHandler prev = this.GetAppHandler();
             this.AppHandler = appHandler;
             return prev;
-        }
-
-        internal AppHandler GetAppHandler()
-        {
-            return this.AppHandler;
         }
 
         private UserSettings? m_UserSettings = null;
@@ -69,6 +71,17 @@ namespace EZAsesAutoType
                 m_UserSettings = value;
             }
         }
+        private UserSettings GetUserSettings()
+        {
+            return this.UserSettings;
+        }
+        private UserSettings SetUserSettings(UserSettings userSettings)
+        {
+            UserSettings prev = this.GetUserSettings();
+            this.UserSettings = userSettings;
+            return prev;
+        }
+        #endregion
 
         /// <summary>
         ///  Default constructor.
@@ -123,7 +136,7 @@ namespace EZAsesAutoType
                 if (userSettings == null)
                     throw new Exception(nameof(userSettings) + Const.LogIsNull);
 
-                this.m_UserSettings = userSettings;
+                this.SetUserSettings(userSettings);
                 return true;
             }
             catch (Exception ex)
@@ -137,17 +150,191 @@ namespace EZAsesAutoType
             }
         }
 
+        private UserSettings? GetUserSettingsFromGui()
+        {
+            try
+            { 
+                UserSettings userSettings      = new UserSettings();
+                userSettings.ASESBaseUrl       = this.textBoxUrl.Text;
+                userSettings.ASESUserId        = this.textBoxUid.Text;
+                userSettings.ASESPassword      = this.textBoxPwd.Text;
+                userSettings.ASESClientDefault = this.comboBoxClientNo.Text;
+                userSettings.ASESPunchIn       = this.textBoxPunchIn.Text; 
+                userSettings.ASESPunchOut      = this.textBoxPunchOut.Text;
+                userSettings.WebDriverDefault  = this.comboBoxWebDriver.Text;
+                return userSettings;
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex);
+                return null;
+            }
+            finally
+            {
+                Log.Debug(Const.LogDone);
+            }        
+        }
+
         private bool SaveUserSettings()
         {
             try
             {
                 Log.Debug(Const.LogStart);
-                if (this.m_UserSettings == null)
-                    throw new Exception(nameof(this.m_UserSettings) + Const.LogIsNull);
+                UserSettings? userSettings = this.GetUserSettings();
+                if (userSettings == null)
+                    throw new Exception(nameof(userSettings) + Const.LogIsNull);
 
-                if (!AppHandler.SaveUserSettings(this.m_UserSettings))
+                // map only a subset of the usersettings
+                UserSettings? userSettingsGui = this.GetUserSettingsFromGui();
+                if (userSettingsGui == null)
+                    throw new Exception(nameof(userSettingsGui) + Const.LogIsNull);
+
+
+                if (!AppHandler.SaveUserSettings(userSettings))
                     throw new Exception(nameof(AppHandler.SaveUserSettings) + Const.LogFail);
 
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex);
+                return false;
+            }
+            finally
+            {
+                Log.Debug(Const.LogDone);
+            }
+        }
+
+        private bool InitializeComboBoxClientNo(UserSettings userSettings)
+        {
+            try
+            {
+                Log.Debug(Const.LogStart);
+                if (userSettings == null)
+                    throw new ArgumentNullException(nameof(userSettings));
+
+                StringCollection? itemList = userSettings.ASESClientList;
+                if (itemList == null)
+                    throw new Exception(nameof(itemList) + Const.LogIsNull);
+
+                ComboBox comboBox = this.comboBoxClientNo;
+                if (comboBox == null)
+                    throw new Exception(nameof(comboBox) + Const.LogIsNull);
+
+                comboBox.Items.Clear();
+                foreach (string? item in itemList)
+                    if (!string.IsNullOrEmpty(item))
+                        this.comboBoxClientNo.Items.Add(item);
+    
+                string? defautValue = userSettings.ASESClientDefault;
+                if (string.IsNullOrEmpty(defautValue))
+                {
+                    if (comboBox.Items.Count > 0)
+                    {
+                        object? firstItem = comboBox.Items[0];
+                        if (firstItem != null)
+                            defautValue = (string)firstItem;
+                    }
+                }
+                comboBox.Text = defautValue;
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex);
+                return false;
+            }
+            finally
+            {
+                Log.Debug(Const.LogDone);
+            }
+        }
+
+        private bool InitializeComboBoxWebDriver(UserSettings userSettings)
+        {
+            try
+            {
+                Log.Debug(Const.LogStart);
+                if (userSettings == null)
+                    throw new ArgumentNullException(nameof(userSettings));
+
+                StringCollection? itemList = userSettings.WebDriverList;
+                if (itemList == null)
+                    throw new Exception(nameof(itemList) + Const.LogIsNull);
+
+                ComboBox comboBox = this.comboBoxWebDriver;
+                if (comboBox == null)
+                    throw new Exception(nameof(comboBox) + Const.LogIsNull);
+
+                comboBox.Items.Clear();
+                foreach (string? item in itemList)
+                    if (!string.IsNullOrEmpty(item))
+                        comboBox.Items.Add(item);
+
+                string? defautValue = userSettings.WebDriverDefault;
+                if (string.IsNullOrEmpty(defautValue))
+                {
+                    if (comboBox.Items.Count > 0)
+                    {
+                        object? firstItem = comboBox.Items[0];
+                        if (firstItem != null)
+                            defautValue = (string)firstItem;
+                    }
+                }
+                comboBox.Text = defautValue;
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex);
+                return false;
+            }
+            finally
+            {
+                Log.Debug(Const.LogDone);
+            }
+        }
+
+        private bool InitializeComboBoxes(UserSettings userSettings)
+        {
+            try
+            {
+                Log.Debug(Const.LogStart);
+                if(userSettings == null)
+                    throw new ArgumentNullException(nameof(userSettings));
+
+                if (!InitializeComboBoxClientNo(userSettings))
+                    throw new Exception(nameof(InitializeComboBoxClientNo) + Const.LogFail);
+
+                if(!InitializeComboBoxWebDriver(userSettings))
+                    throw new Exception(nameof(InitializeComboBoxWebDriver) + Const.LogFail);
+                
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex);
+                return false;
+            }
+            finally
+            {
+                Log.Debug(Const.LogDone);
+            }
+        }
+
+        private bool RenderControls()
+        {
+            try
+            {
+                Log.Debug(Const.LogStart);
+                UserSettings? userSettings = this.GetUserSettings();
+                if (userSettings == null)
+                    throw new Exception(nameof(userSettings) + Const.LogIsNull);
+
+                
                 return true;
             }
             catch (Exception ex)
@@ -171,6 +358,13 @@ namespace EZAsesAutoType
                 Log.Debug(Const.LogStart);
                 if (!LoadUserSettings())
                     throw new Exception(nameof(LoadUserSettings) + Const.LogFail);
+
+                UserSettings userSettings = this.GetUserSettings();
+                if (userSettings == null)
+                    throw new Exception(nameof(userSettings) + Const.LogIsNull);
+
+                if (!InitializeComboBoxes(userSettings))
+                    throw new Exception(nameof(InitializeComboBoxes) + Const.LogFail);
 
             }
             catch (Exception ex)
@@ -226,11 +420,6 @@ namespace EZAsesAutoType
 
         #endregion
 
-
-        private void textBox1_Uid_TextChanged(object sender, EventArgs e)
-        {
-
-        }
     } // class
 
 } // namespace
