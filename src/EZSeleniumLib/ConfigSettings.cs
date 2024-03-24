4ea08930e4ae2c553981e7ed492fe6299728fbb1
Tomislav Matas <slove@matas.de>
2024-03-24 12:04:09 +0100

    main Version "24.123.0.0" Massive refactoring within "EZSeleniumLib" towards "clean code" and "seperation of concerns".

--- src/EZSeleniumLib/ConfigSettings.cs
+++ src/EZSeleniumLib/ConfigSettings.cs
@@ -1,110 +1,98 @@
 ﻿//
-// File: LibConfig.cs
+// File: "ConfigSettings.cs"
 //
 // Summary:
-// Project specific configuration settings. 
+// Assembly specific configuration settings.
 //
 // Revision History: 
-// 2024/03/22:TomislavMatas: Version "24.123.0.0"
+// 2024/03/24:TomislavMatas: Version "24.123.0.0"
 // * Initial version.
 //
-
-using ConfigApi = EZSeleniumLib.ConfigApi;
 
 namespace EZSeleniumLib
 {
     /// <summary>
-    /// Project specific constants.
+    /// Assembly specific configuration settings.
     /// </summary>
     public static class ConfigSettings
     {
-        #region "ÊZSeleniumLib.Browser.WebDriver"
-        public const string BrowserImplementationChrome = "Chrome";
-        public const string BrowserImplementationEdge = "Edge";
-        private const string BrowserImplementationName = "ÊZSeleniumLib.Browser.WebDriver";
-        private const string BrowserImplementationDefault = BrowserImplementationChrome;
-        private static string? m_BrowserImplementation = null;
-        private static string BrowserImplementation
+        #region "EZSeleniumLib.WebDriver"
+        private static string? m_WebDriver = null;
+        private static string WebDriver
         {
             get
             {
-                if (m_BrowserImplementation == null)
-                    m_BrowserImplementation = ConfigApi.GetAppSettingString(BrowserImplementationName, BrowserImplementationDefault);
-                return m_BrowserImplementation;
+                if (m_WebDriver == null)
+                    m_WebDriver = ConfigApi.GetAppSettingString(Const.WebDriverKeyName, Const.WebDriverDefault);
+                return m_WebDriver;
             }
             set
             {
-                m_BrowserImplementation = value;
+                m_WebDriver = value;
             }
         }
-        public static string GetBrowserImplementation()
+        public static string GetWebDriver()
         {
-            return BrowserImplementation;
+            return WebDriver;
         }
-        public static string SetBrowserImplementation(string value)
+        public static string SetWebDriver(string value)
         {
-            string prev = BrowserImplementation;
-            BrowserImplementation = value;
+            string prev = WebDriver;
+            WebDriver = value;
             return prev;
         }
         #endregion 
 
-        #region "ÊZSeleniumLib.Browser.InitMode"
-        public const string BrowserInitModeSimple = "simple";
-        public const string BrowserInitModeExtended = "extended";
-        private const string BrowserInitModeKeyName = "ÊZSeleniumLib.Browser.InitMode";
-        private const string BrowserInitModeDefault = BrowserInitModeExtended;
-        private static string? m_BrowserInitMode = null;
-        private static string BrowserInitMode
+        #region "EZSeleniumLib.WebDriver.InitMode"
+        private static string? m_WebDriverInitMode = null;
+        private static string WebDriverInitMode
         {
             get
             {
-                if (m_BrowserInitMode == null)
-                    m_BrowserInitMode = ConfigApi.GetAppSettingString(BrowserInitModeKeyName, BrowserInitModeDefault);
-                return m_BrowserInitMode;
+                if (m_WebDriverInitMode == null)
+                    m_WebDriverInitMode = ConfigApi.GetAppSettingString(Const.WebDriverInitModeKeyName, Const.WebDriverInitModeDefault);
+                return m_WebDriverInitMode;
             }
             set
             {
-                m_BrowserInitMode = value;
+                m_WebDriverInitMode = value;
             }
         }
-        public static string GetBrowserInitMode()
+        public static string GetWebDriverInitMode()
         {
-            return BrowserInitMode;
+            return WebDriverInitMode;
         }
-        public static string SetBrowserInitMode(string value)
+        public static string SetWebDriverInitMode(string value)
         {
-            string prev = BrowserInitMode;
-            BrowserInitMode = value;
+            string prev = WebDriverInitMode;
+            WebDriverInitMode = value;
             return prev;
         }
         #endregion 
 
-        #region "ÊZSeleniumLib.Browser.Interaction.Delay"
-        private const string BrowserInteractionDelayKeyName = "ÊZSeleniumLib.Browser.Interaction.Delay";
-        private const int BrowserInteractionDelayDefault = 500;
-        private static int? m_BrowserInteractionDelay = null;
-        private static int BrowserInteractionDelay
+        #region "EZSeleniumLib.WebDriver.Delay"
+        private static int? m_WebDriverDelay = null;
+        private static int WebDriverDelay
         {
             get
             {
-                if (m_BrowserInteractionDelay == null)
-                    m_BrowserInteractionDelay = ConfigApi.GetAppSettingInt(BrowserInteractionDelayKeyName, BrowserInteractionDelayDefault);
-                return (int)m_BrowserInteractionDelay;
+                if (m_WebDriverDelay == null)
+                    m_WebDriverDelay = ConfigApi.GetAppSettingInt(Const.WebDriverDelayKeyName, Const.WebDriverDelayDefault);
+                return (int)m_WebDriverDelay;
             }
             set
             {
-                m_BrowserInteractionDelay = value;
+                m_WebDriverDelay = value;
             }
         }
-        public static int GetBrowserInteractionDelay()
+        public static int GetWebDriverDelay()
         {
-            return BrowserInteractionDelay;
+            return WebDriverDelay;
         }
-        public static int SetBrowserInteractionDelay(int value)
+        public static int SetWebDriverDelay(int value)
         {
-            int prev = BrowserInteractionDelay;
-            BrowserInteractionDelay = value;
+            int prev = WebDriverDelay;
+            WebDriverDelay = value;
             return prev;
         }
         #endregion 
