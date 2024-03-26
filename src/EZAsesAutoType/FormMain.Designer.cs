a62d3a2af48c763ca8feb782d7078ea8acce17f2
Tomislav Matas <tomislav.matas@deutschebahn.com>
2024-03-26 22:34:08 +0100

    main Version "1.0.0.0" Implement "Cancel" Button.

--- src/EZAsesAutoType/FormMain.Designer.cs
+++ src/EZAsesAutoType/FormMain.Designer.cs
@@ -49,14 +49,15 @@
             labelLanguage = new Label();
             labelDash1 = new Label();
             backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            btnCancel = new Button();
             SuspendLayout();
             // 
             // btnRun
             // 
-            btnRun.Location = new Point(11, 168);
-            btnRun.Margin = new Padding(2);
+            btnRun.Location = new Point(10, 192);
+            btnRun.Margin = new Padding(2, 3, 2, 3);
             btnRun.Name = "btnRun";
-            btnRun.Size = new Size(459, 43);
+            btnRun.Size = new Size(463, 52);
             btnRun.TabIndex = 30;
             btnRun.Text = "Run";
             btnRun.UseVisualStyleBackColor = true;
@@ -64,8 +65,8 @@
             // 
             // textBoxUrl
             // 
-            textBoxUrl.Location = new Point(97, 10);
-            textBoxUrl.Margin = new Padding(2);
+            textBoxUrl.Location = new Point(97, 11);
+            textBoxUrl.Margin = new Padding(2, 3, 2, 3);
             textBoxUrl.Name = "textBoxUrl";
             textBoxUrl.Size = new Size(373, 27);
             textBoxUrl.TabIndex = 40;
@@ -73,8 +74,8 @@
             // 
             // textBoxUid
             // 
-            textBoxUid.Location = new Point(97, 42);
-            textBoxUid.Margin = new Padding(2);
+            textBoxUid.Location = new Point(97, 47);
+            textBoxUid.Margin = new Padding(2, 3, 2, 3);
             textBoxUid.Name = "textBoxUid";
             textBoxUid.Size = new Size(140, 27);
             textBoxUid.TabIndex = 50;
@@ -82,8 +83,8 @@
             // 
             // textBoxPwd
             // 
-            textBoxPwd.Location = new Point(330, 42);
-            textBoxPwd.Margin = new Padding(2);
+            textBoxPwd.Location = new Point(330, 47);
+            textBoxPwd.Margin = new Padding(2, 3, 2, 3);
             textBoxPwd.Name = "textBoxPwd";
             textBoxPwd.PasswordChar = '*';
             textBoxPwd.Size = new Size(140, 27);
@@ -93,8 +94,8 @@
             // 
             // textBoxPunchIn
             // 
-            textBoxPunchIn.Location = new Point(97, 137);
-            textBoxPunchIn.Margin = new Padding(2);
+            textBoxPunchIn.Location = new Point(97, 156);
+            textBoxPunchIn.Margin = new Padding(2, 3, 2, 3);
             textBoxPunchIn.Name = "textBoxPunchIn";
             textBoxPunchIn.Size = new Size(58, 27);
             textBoxPunchIn.TabIndex = 10;
@@ -103,8 +104,8 @@
             // 
             // textBoxPunchOut
             // 
-            textBoxPunchOut.Location = new Point(179, 137);
-            textBoxPunchOut.Margin = new Padding(2);
+            textBoxPunchOut.Location = new Point(179, 155);
+            textBoxPunchOut.Margin = new Padding(2, 3, 2, 3);
             textBoxPunchOut.Name = "textBoxPunchOut";
             textBoxPunchOut.Size = new Size(58, 27);
             textBoxPunchOut.TabIndex = 20;
@@ -114,7 +115,7 @@
             // labelUrl
             // 
             labelUrl.AutoSize = true;
-            labelUrl.Location = new Point(10, 12);
+            labelUrl.Location = new Point(10, 16);
             labelUrl.Margin = new Padding(2, 0, 2, 0);
             labelUrl.Name = "labelUrl";
             labelUrl.Size = new Size(38, 20);
@@ -124,7 +125,7 @@
             // labelUid
             // 
             labelUid.AutoSize = true;
-            labelUid.Location = new Point(10, 45);
+            labelUid.Location = new Point(10, 52);
             labelUid.Margin = new Padding(2, 0, 2, 0);
             labelUid.Name = "labelUid";
             labelUid.Size = new Size(56, 20);
@@ -134,7 +135,7 @@
             // labelPwd
             // 
             labelPwd.AutoSize = true;
-            labelPwd.Location = new Point(249, 45);
+            labelPwd.Location = new Point(249, 52);
             labelPwd.Margin = new Padding(2, 0, 2, 0);
             labelPwd.Name = "labelPwd";
             labelPwd.Size = new Size(73, 20);
@@ -144,7 +145,7 @@
             // labelPunchIn
             // 
             labelPunchIn.AutoSize = true;
-            labelPunchIn.Location = new Point(11, 140);
+            labelPunchIn.Location = new Point(11, 160);
             labelPunchIn.Margin = new Padding(2, 0, 2, 0);
             labelPunchIn.Name = "labelPunchIn";
             labelPunchIn.Size = new Size(62, 20);
@@ -156,8 +157,8 @@
             comboBoxClientNo.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
             comboBoxClientNo.AutoCompleteSource = AutoCompleteSource.ListItems;
             comboBoxClientNo.FormattingEnabled = true;
-            comboBoxClientNo.Location = new Point(97, 73);
-            comboBoxClientNo.Margin = new Padding(2);
+            comboBoxClientNo.Location = new Point(97, 83);
+            comboBoxClientNo.Margin = new Padding(2, 3, 2, 3);
             comboBoxClientNo.Name = "comboBoxClientNo";
             comboBoxClientNo.Size = new Size(140, 28);
             comboBoxClientNo.TabIndex = 70;
@@ -169,7 +170,7 @@
             // labelClientNo
             // 
             labelClientNo.AutoSize = true;
-            labelClientNo.Location = new Point(10, 76);
+            labelClientNo.Location = new Point(10, 88);
             labelClientNo.Margin = new Padding(2, 0, 2, 0);
             labelClientNo.Name = "labelClientNo";
             labelClientNo.Size = new Size(50, 20);
@@ -180,8 +181,8 @@
             // 
             comboBoxWebDriver.DropDownStyle = ComboBoxStyle.DropDownList;
             comboBoxWebDriver.FormattingEnabled = true;
-            comboBoxWebDriver.Location = new Point(97, 105);
-            comboBoxWebDriver.Margin = new Padding(2);
+            comboBoxWebDriver.Location = new Point(97, 119);
+            comboBoxWebDriver.Margin = new Padding(2, 3, 2, 3);
             comboBoxWebDriver.Name = "comboBoxWebDriver";
             comboBoxWebDriver.Size = new Size(140, 28);
             comboBoxWebDriver.TabIndex = 90;
@@ -190,7 +191,7 @@
             // labelWebDriver
             // 
             labelWebDriver.AutoSize = true;
-            labelWebDriver.Location = new Point(10, 107);
+            labelWebDriver.Location = new Point(10, 124);
             labelWebDriver.Margin = new Padding(2, 0, 2, 0);
             labelWebDriver.Name = "labelWebDriver";
             labelWebDriver.Size = new Size(82, 20);
@@ -200,7 +201,7 @@
             // labelWebDriverVersion
             // 
             labelWebDriverVersion.AutoSize = true;
-            labelWebDriverVersion.Location = new Point(249, 107);
+            labelWebDriverVersion.Location = new Point(249, 124);
             labelWebDriverVersion.Margin = new Padding(2, 0, 2, 0);
             labelWebDriverVersion.Name = "labelWebDriverVersion";
             labelWebDriverVersion.Size = new Size(60, 20);
@@ -211,8 +212,8 @@
             // 
             textBoxWebDriverVersion.BorderStyle = BorderStyle.FixedSingle;
             textBoxWebDriverVersion.Enabled = false;
-            textBoxWebDriverVersion.Location = new Point(330, 105);
-            textBoxWebDriverVersion.Margin = new Padding(2);
+            textBoxWebDriverVersion.Location = new Point(330, 119);
+            textBoxWebDriverVersion.Margin = new Padding(2, 3, 2, 3);
             textBoxWebDriverVersion.Name = "textBoxWebDriverVersion";
             textBoxWebDriverVersion.ReadOnly = true;
             textBoxWebDriverVersion.Size = new Size(140, 27);
@@ -223,7 +224,7 @@
             // 
             comboBoxLanguage.DropDownStyle = ComboBoxStyle.DropDownList;
             comboBoxLanguage.FormattingEnabled = true;
-            comboBoxLanguage.Location = new Point(330, 73);
+            comboBoxLanguage.Location = new Point(330, 83);
             comboBoxLanguage.Name = "comboBoxLanguage";
             comboBoxLanguage.Size = new Size(140, 28);
             comboBoxLanguage.TabIndex = 80;
@@ -231,7 +232,7 @@
             // labelLanguage
             // 
             labelLanguage.AutoSize = true;
-            labelLanguage.Location = new Point(249, 76);
+            labelLanguage.Location = new Point(249, 88);
             labelLanguage.Margin = new Padding(2, 0, 2, 0);
             labelLanguage.Name = "labelLanguage";
             labelLanguage.Size = new Size(77, 20);
@@ -241,7 +242,7 @@
             // labelDash1
             // 
             labelDash1.AutoSize = true;
-            labelDash1.Location = new Point(156, 140);
+            labelDash1.Location = new Point(155, 160);
             labelDash1.Margin = new Padding(2, 0, 2, 0);
             labelDash1.Name = "labelDash1";
             labelDash1.Size = new Size(23, 20);
@@ -252,11 +253,24 @@
             // 
             backgroundWorker1.DoWork += backgroundWorker1_DoWork;
             // 
+            // btnCancel
+            // 
+            btnCancel.Enabled = false;
+            btnCancel.Location = new Point(10, 192);
+            btnCancel.Name = "btnCancel";
+            btnCancel.Size = new Size(464, 52);
+            btnCancel.TabIndex = 101;
+            btnCancel.Text = "Cancel";
+            btnCancel.UseVisualStyleBackColor = true;
+            btnCancel.Visible = false;
+            btnCancel.Click += btnCancel_Click;
+            // 
             // FormMain
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(484, 224);
+            ClientSize = new Size(486, 259);
+            Controls.Add(btnCancel);
             Controls.Add(labelDash1);
             Controls.Add(labelLanguage);
             Controls.Add(comboBoxLanguage);
@@ -278,9 +292,7 @@
             Controls.Add(btnRun);
             Icon = (Icon)resources.GetObject("$this.Icon");
             MaximizeBox = false;
-            MaximumSize = new Size(502, 271);
             MinimizeBox = false;
-            MinimumSize = new Size(502, 271);
             Name = "FormMain";
             Text = "EZAsesAutoType";
             FormClosing += onClosing;
@@ -311,5 +323,6 @@
         private Label labelLanguage;
         private Label labelDash1;
         private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private Button btnCancel;
     }
 }
