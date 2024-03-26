edb592fc0e6529e4ce107b6b0870bb9719598df6
Tomislav Matas <tomislav.matas@deutschebahn.com>
2024-03-26 01:56:41 +0100

    main Version "1.0.0.0" Prototyping Navigation with * "chromedriver.exe" version "123.0.6312.58". * "MicrosoftWebDriver.exe" version "122.0.2365.106". * "geckodriver.exe" version "0.34.0".

--- src/EZAsesAutoType/Properties/Settings.Designer.cs
+++ src/EZAsesAutoType/Properties/Settings.Designer.cs
@@ -180,7 +180,7 @@ namespace EZAsesAutoType.Properties {
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute("<?xml version=\"1.0\" encoding=\"utf-16\"?>\r\n<ArrayOfString xmlns:xsd=\"http://www.w3." +
             "org/2001/XMLSchema\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">\r\n  <s" +
-            "tring>Chrome</string>\r\n  <string>Edge</string>\r\n</ArrayOfString>")]
+            "tring>Chrome</string>\r\n  <string>Edge</string>\r\n  <string>Firefox</string>\r\n</ArrayOfString>")]
         public global::System.Collections.Specialized.StringCollection WebDriverList {
             get {
                 return ((global::System.Collections.Specialized.StringCollection)(this["WebDriverList"]));
---
+++ src/EZAsesAutoType/Worker.cs
@@ -0,0 +1,232 @@
+﻿//
+// File: "WorkerThread.cs"
+//
+// Revision History: 
+// 2024/03/22:TomislavMatas: Version "1.0.0.0"
+// * Initial version.
+//
+
+using EZSeleniumLib;
+using log4net;
+using OpenQA.Selenium;
+
+namespace EZAsesAutoType
+{
+    /// <summary>
+    ///  This class shall implement all functionality 
+    ///  that originate from any dialog or form using 
+    ///  dedicated worker methodz. Obay genral rule, that
+    ///  Windows Apps shall always be designed with 
+    ///  multi-threaded approach to prevent "freezing".
+    /// </summary>
+    internal class Worker
+    {
+        #region log4net
+        private static ILog? m_Log = null;
+        private static ILog Log
+        {
+            get
+            {
+                if (m_Log == null)
+                    m_Log = LogManager.GetLogger(typeof(Worker));
+                return m_Log;
+            }
+        }
+        #endregion
+
+        #region propertiez
+        private WorkerConfig? m_WorkerConfig = null;
+        private WorkerConfig WorkerConfig
+        {
+            get
+            {
+                if (this.m_WorkerConfig == null)
+                    this.m_WorkerConfig = new WorkerConfig();
+                return this.m_WorkerConfig;
+            }
+            set 
+            {
+                this.m_WorkerConfig = value;
+            }
+        }
+        public WorkerConfig GetWorkerConfig()
+        {
+            return this.WorkerConfig;
+        }
+        public WorkerConfig SetWorkerConfig(WorkerConfig workerConfig)
+        {
+            WorkerConfig prev = this.GetWorkerConfig();
+            this.WorkerConfig = workerConfig;
+            return prev;
+        }
+        #endregion
+
+        #region Instantiation
+
+        /// <summary>
+        /// Initialize instance.
+        /// </summary>
+        /// <param name="workerConfig"></param>
+        /// <returns></returns>
+        private bool Initialze(WorkerConfig workerConfig)
+        {
+            try
+            {
+                Log.Debug(Const.LogStart);
+                this.SetWorkerConfig(workerConfig);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex);
+                return false;
+            }
+            finally
+            {
+                Log.Debug(Const.LogDone);
+            }
+        }
+
+        /// <summary>
+        ///  Default constructor.
+        /// </summary>
+        public Worker()
+        {
+            try
+            {
+                Log.Debug(Const.LogStart);
+                if(!Initialze(new WorkerConfig()))
+                    throw new Exception(nameof(Initialze) + Const.LogFail);
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex);
+            }
+            finally
+            {
+                Log.Debug(Const.LogDone);
+            }
+        }
+
+        /// <summary>
+        ///  Custom constructor.
+        /// </summary>
+        public Worker(WorkerConfig workerConfig)
+        {
+            try
+            {
+                Log.Debug(Const.LogStart);
+                if(!Initialze(workerConfig))
+                    throw new Exception(nameof(Initialze) + Const.LogFail);
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex);
+            }
+            finally
+            {
+                Log.Debug(Const.LogDone);
+            }
+        }
+
+        #endregion
+
+        #region WebDriver 
+
+        private string GetBaseUrl()
+        {
+            return this.WorkerConfig.GetUserSettings().ASESBaseUrl;
+        }
+
+        /// <summary>
+        /// Return a specific descendant of class "BrowserBase" using "BrowserFactory".
+        /// </summary>
+        /// <param name="webDriver"></param>
+        /// <param name="browserOptions"></param>
+        /// <returns></returns>
+        private BrowserBase GetBrowserInstance(string webDriver, BrowserOptions browserOptions)
+        {
+            try
+            {
+                Log.Debug(Const.LogStart);
+                return BrowserFactory.GetBrowserInstance(webDriver, browserOptions);
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex);
+                return null;
+            }
+            finally
+            {
+                Log.Debug(Const.LogDone);
+            }
+        }
+
+        /// <summary>
+        /// Use Browser-Interop Helper to navigate to "Login Page".
+        /// </summary>
+        /// <param name="browser"></param>
+        /// <param name="timeout">Timeout in seconds.</param>
+        /// <returns></returns>
+        private bool ASESNavigateToLoginPage(BrowserBase browser, string baseUrl, int timeout)
+        {
+            try
+            {
+                Log.Debug(Const.LogStart);
+                if (!browser.GoToUrl(baseUrl))
+                    throw new Exception(String.Format("GoToUrl '{0}' failed", baseUrl));
+
+                #region validate complete rendering 
+                //string xPath = this.GetWawXpathIntialChatPageH1();
+                //IWebElement element = browser.FindElement(By.XPath(xPath), timeout);
+                //if (element == null)
+                //    throw new Exception(String.Format("Element '{0}' not found", xPath));
+                #endregion
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex);
+                return false;
+            }
+            finally
+            {
+                Log.Debug(Const.LogDone);
+            }
+        }
+
+        #endregion
+
+        public bool DoDailyPunch(UserSettings userSettings)
+        {
+            try
+            {
+                Log.Debug(Const.LogStart);
+                int timeout = 60; // seconds
+                string webDriver = userSettings.WebDriver;
+                BrowserOptions browserOptions = new BrowserOptions();
+                BrowserBase browser = this.GetBrowserInstance(webDriver, browserOptions);
+                if (browser == null)
+                    throw new Exception(nameof(this.GetBrowserInstance) + Const.LogFail);
+
+                string baseUrl = userSettings.ASESBaseUrl;
+                if (!this.ASESNavigateToLoginPage(browser, baseUrl,timeout))
+                    throw new Exception(nameof(this.ASESNavigateToLoginPage) + Const.LogFail);
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex);
+                return false;
+            }
+            finally
+            {
+                Log.Debug(Const.LogDone);
+            }
+        }
+
+    } // class
+
+} // namespace
