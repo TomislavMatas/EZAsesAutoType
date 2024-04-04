80b69428045c3843703992ec889502ab1bef6840
Tomislav Matas <tomislav.matas@deutschebahn.com>
2024-04-04 16:14:26 +0200

    webdriver/123 Version "1.0.123" Edit "updateWebdriverEdge.bat": * Set "WEBDRIVER_VERSION_DEFAULT=123.0.2420.65" * Tidy~Up commentz.

--- bat/updateWebdriverEdge.bat
+++ bat/updateWebdriverEdge.bat
@@ -8,10 +8,13 @@
 :: -- 
 :: -- Example:
 :: -- ```
-:: -- CMD> updateWebdriverEdge 124.0.2464.2
+:: -- CMD> updateWebdriverEdge 123.0.2420.65
 :: -- ```
 :: --
 :: -- Revision History
+:: -- 2024/04/04:TomislavMatas: Version "1.0.123"
+:: -- * Set "WEBDRIVER_VERSION_DEFAULT=123.0.2420.65"
+:: -- * Tidy~Up commentz.
 :: -- 2024/03/25:TomislavMatas: Version "1.0.0.0"
 :: -- * Inital version with default webdriver version "122.0.2365.106".
 :: --
@@ -19,7 +22,7 @@
 @echo off
 setlocal EnableExtensions EnableDelayedExpansion
 
-set "WEBDRIVER_VERSION_DEFAULT=122.0.2365.106"
+set "WEBDRIVER_VERSION_DEFAULT=123.0.2420.65"
  
 echo see current versions of webdriver at:
 echo https://developer.microsoft.com/de-de/microsoft-edge/tools/webdriver/
