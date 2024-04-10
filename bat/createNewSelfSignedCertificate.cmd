c5a1f980f98c4a468e73b4b23c6617a4bd5ae50a
Tomislav Matas <tomislav.matas@deutschebahn.com>
2024-04-11 00:40:02 +0200

    webdriver/123 version "1.123.4.0" Prototyping code signage of release msi with self signed certificate.

--- bat/createNewSelfSignedCertificate.cmd
+++ bat/createNewSelfSignedCertificate.cmd
@@ -21,9 +21,15 @@ set "PSSCRIPT=%PSSCRIPT% -KeyUsage DigitalSignature"
 set "PSSCRIPT=%PSSCRIPT% -FriendlyName \"matas consulting selfsigned\""
 set "PSSCRIPT=%PSSCRIPT% -CertStoreLocation \"Cert:\\CurrentUser\\My\""
 set "PSSCRIPT=%PSSCRIPT% -TextExtension @(\"2.5.29.37={text}1.3.6.1.5.5.7.3.3\", \"2.5.29.19={text}\")"
+
+@echo on
 %PSLAUNCH% %PSSCRIPT%
-if errorlevel 1 (
-	echo ERROR "%PSSCRIPT%" failed
-	goto done
+@if errorlevel 1 @(
+	@echo ERROR "%PSSCRIPT%" failed
+	@goto done
 )
+@echo A new self signed certificate has been created in your personal certificate store.
+@echo Export it as "MatasConsultingSelfSigned.pfx" with a secret password to the
+@echo directory "%~dp0..\cert" to make it available for scripted code signage.
+
 :done
