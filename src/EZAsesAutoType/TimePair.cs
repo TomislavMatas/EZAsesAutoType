609e28f8fce3181b905fd131e439c91d29b570bd
Tomislav Matas <slove@matas.de>
2024-04-12 00:49:23 +0200

    webdriver/123 Version "1.123.4.0" Prototyping "multiple time pairs" entry.

---
+++ src/EZAsesAutoType/TimePair.cs
@@ -0,0 +1,45 @@
+﻿//
+// File: "TimePair.cs"
+//
+// Revision History: 
+// 2024/04/12:TomislavMatas: Version "1.123.4.0"
+// * Initial version.
+//
+
+namespace EZAsesAutoType
+{
+    internal class TimePair
+    {
+        public string? PunchIn = null;
+        public string? PunchOut = null;
+
+        /// <summary>
+        /// Init instance.
+        /// </summary>
+        /// <param name="punchIn"></param>
+        /// <param name="punchOut"></param>
+        private void Initialize(string? punchIn, string? punchOut)
+        {
+            this.PunchIn = punchIn;
+            this.PunchOut = punchOut;
+        }
+
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        public TimePair()
+        {
+            this.Initialize(null, null);
+        }
+
+        /// <summary>
+        /// Custom constructor.
+        /// </summary>
+        public TimePair(string? punchIn = null, string? punchOut = null)
+        {
+            this.Initialize(punchIn, punchOut);
+        }
+
+    } // class
+
+} // namespace
