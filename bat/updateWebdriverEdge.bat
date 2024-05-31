b7400907302383c5df7b2e2b74bb77b82b11f6f3
Tomislav Matas <tomislav.matas@deutschebahn.com>
2024-05-31 02:24:40 +0200

    webdriver/126 version "1.126.0" Edit "bat/updateWebdriverEdge.bat" Custom renaming from "msedgedriver.exe" to "MicrosoftWebDriver.exe" is obsolete when using Selenium version 4 (ff).

--- bat/updateWebdriverEdge.bat
+++ bat/updateWebdriverEdge.bat
@@ -12,6 +12,10 @@
 :: -- ```
 :: --
 :: -- Revision History
+:: -- 2024/05/31:TomislavMatas: Version "1.126.0"
+:: -- * Custom renaming from "msedgedriver.exe" to 
+:: --   "MicrosoftWebDriver.exe" is obsolete 
+:: --   when using Selenium version 4 (ff).
 :: -- 2024/05/03:TomislavMatas: Version "1.125.0"
 :: -- * Set "WEBDRIVER_VERSION_DEFAULT=125.0.2535.13"
 :: -- 2024/05/02:TomislavMatas: Version "1.124.0"
@@ -28,11 +32,13 @@ setlocal EnableExtensions EnableDelayedExpansion
 
 set "WEBDRIVER_VERSION_DEFAULT=125.0.2535.13"
 
+set "WEBDRIVER_HOMEPAGE=https://developer.microsoft.com/de-de/microsoft-edge/tools/webdriver"
+
 echo see browser release schedule at:
 echo https://learn.microsoft.com/de-de/deployedge/microsoft-edge-release-schedule/
 
 echo see available versions of webdriver at:
-echo https://developer.microsoft.com/de-de/microsoft-edge/tools/webdriver/
+echo %WEBDRIVER_HOMEPAGE%
 
 pause
 
@@ -50,16 +56,21 @@ if "%WEBDRIVER_VERSION%" == "" (
 	set "WEBDRIVER_VERSION=%WEBDRIVER_VERSION_DEFAULT%"
 )
 
-set "DOWNLOAD_URL=%WEBDRIVER_HOMEPAGE%/%WEBDRIVER_VERSION%"
+set "PROJECT_ROOT=%~dp0.."
+set "SOURCE_ROOT=%PROJECT_ROOT%\src"
+set "DOWNLOAD_CDN=https://msedgedriver.azureedge.net"
+set "DOWNLOAD_URL=%DOWNLOAD_CDN%/%WEBDRIVER_VERSION%"
 set "DOWNLOAD_ROOT=%PROJECT_ROOT%\bin\WebDriver\Edge"
 set "DOWNLOAD_DIR=%DOWNLOAD_ROOT%\%WEBDRIVER_VERSION%"
 @rem die Liste der Dateien, die heruntergeladen werden sollen.
 ::set "FILELIST=edgedriver_win64.zip edgedriver_win32.zip edgedriver_linux64.zip edgedriver_arm64.zip"
 set "FILELIST=edgedriver_win64.zip"
-@rem Die Umbenennung ist nur fuer die Windows Treiber erforderlich, 
-@rem weil aktuell noch ein veraltetes Selenium-Framework verwendet wird.
+@rem Die Umbenennung fuer den msedgedriver Treiber war fuer Selenium 3 erforderlich.
+@rem Ab Selenium 4 ist die Umbennung nicht mehr erforderlich,
+@rem darum custom name = original name.
 set "FILENAME_ORIGINAL=msedgedriver.exe"
-set "FILENAME_CUSTOM=MicrosoftWebDriver.exe"
+@rem set "FILENAME_CUSTOM=MicrosoftWebDriver.exe"
+set "FILENAME_CUSTOM=%FILENAME_ORIGINAL%"
 set "CURL_EXE=curl"
 set "CURL_OPT=-k -sS --fail"
 
@@ -73,10 +84,10 @@ for %%f in (%FILELIST%) do (
 	echo download "%DOWNLOAD_URL%/%%f" ...
 	"%CURL_EXE%" %CURL_OPT% "%DOWNLOAD_URL%/%%f" --output "%DOWNLOAD_DIR%\%%f"
 	if ERRORLEVEL 1 (
-		echo ERROR download "%DOWNLOAD_URL%/%%f" failed
+		echo FAILURE
 		exit /b 2
 	)	
-	echo download "%DOWNLOAD_URL%/%%f" OK
+	echo OK
 )
 
 for %%f in (%FILELIST%) do (
@@ -91,63 +102,77 @@ for %%f in (%FILELIST%) do (
 	echo unzip "!fn!" ...
 	!pscmd! "!script!"
 	if ERRORLEVEL 1 (
-		echo ERROR unzip "!fn!" failed
+		echo FAILURE
 		exit /b 3
 	)	
-	echo unzip "!fn!" OK
+	echo OK
+
 	@rem Zip-Datei verschieben, weil erfolgreich verarbeitet
 	echo move "!fn!" ... 
 	del "!unzipedpath!\!fn!" 1>nul 2>nul
 	move "!zipfile!" "!unzipedpath!" 1>nul
 	if ERRORLEVEL 1 (
-		echo ERROR move "!zipfile!" failed
+		echo FAILURE
 		exit /b 4
 	)	
-	echo move "!fn!" OK
+	echo OK
+
 	@rem Loesche das nicht wirklich benoetigte Verzeichnis "Driver_Notes" aus dem entpackten Verzeichnis.
 	@rem Ausnahme: Das Verzeichnis "Driver_Notes" gibt es in allen Paketen ausser fuer linux.
 	if "x!fn:linux=!" == "x!fn!" ( 
 		echo remove directory "Driver_Notes" ... 
 		rd /s /q "!unzipedpath!\Driver_Notes" 1>nul
 		if ERRORLEVEL 1 (
-			echo remove directory "Driver_Notes" failed
+			echo FAILURE
 			exit /b 5
 		)	
-		echo remove directory "Driver_Notes" OK
+		echo OK
 	)
-	@rem Die Windows-Treiber umbenennen
+
+@rem	echo copy "%FILENAME_ORIGINAL%" to "%SOURCE_ROOT%" ...
+@rem	copy "!unzipedpath!\%FILENAME_ORIGINAL%" "%SOURCE_ROOT%\%FILENAME_ORIGINAL%" 1>nul
+@rem	if ERRORLEVEL 1 (
+@rem		echo FAILURE
+@rem		exit /b 6
+@rem	)	
+@rem	echo OK
+
+	@rem Eine umbenannte Kopie des Windows-Treiber bereitstellen, falls erfoderlich.
 	if not "x!fn:win=!" == "x!fn!" ( 
-		echo rename "%FILENAME_ORIGINAL%" to "%FILENAME_CUSTOM%" ... 
-		del "!unzipedpath!\%FILENAME_CUSTOM%" 1>nul 2>nul
-		ren "!unzipedpath!\%FILENAME_ORIGINAL%" "%FILENAME_CUSTOM%" 
-		if ERRORLEVEL 1 (
-			echo ERROR rename "%FILENAME_ORIGINAL%" to "%FILENAME_CUSTOM%" failed
-			exit /b 6
-		)	
-		echo rename "%FILENAME_ORIGINAL%" to "%FILENAME_CUSTOM%" OK
-	)		
-	@rem "Verteile" den win64 in die Projekt-Verzeichnisse, in denen dieser benoetigt wird.
-	if not "x!fn:win64=!" == "x!fn!" ( 
-		for %%p in (!PROJECT_LIST!) do (
-			set "prj=%%p" 
-			set "prjpath=%PROJECT_ROOT%\src\!prj!"
-			echo copy "%FILENAME_CUSTOM%" to "!prj!" ...
-			del "!prjpath!\%FILENAME_CUSTOM%" 1>nul 2>nul
-			copy "!unzipedpath!\%FILENAME_CUSTOM%" "!prjpath!\%FILENAME_CUSTOM%" 1>nul
+		if not "%FILENAME_ORIGINAL%" == "%FILENAME_CUSTOM%" (
+			echo copy "%FILENAME_CUSTOM%" to "%SOURCE_ROOT%" ...
+			copy "!unzipedpath!\%FILENAME_CUSTOM%" "%SOURCE_ROOT%\%FILENAME_CUSTOM%" 1>nul
 			if ERRORLEVEL 1 (
-				echo ERROR copy "%FILENAME_CUSTOM%" to "!prj!" failed
+				echo FAILURE
 				exit /b 7
 			)	
-			echo copy "%FILENAME_CUSTOM%" to "!prj!" OK
+			echo OK
+			@rem "Verteile" den win64 in die PROJECT-Verzeichnisse, in denen dieser benoetigt wird.
+			if not "x!fn:win64=!" == "x!fn!" ( 
+				for %%p in (!PROJECT_LIST!) do (
+					set "prj=%%p" 
+					set "prjpath=%PROJECT_ROOT%\src\!prj!"
+					echo copy "%FILENAME_CUSTOM%" to "!prj!" ...
+					del "!prjpath!\%FILENAME_CUSTOM%" 1>nul 2>nul
+					copy "!unzipedpath!\%FILENAME_CUSTOM%" "!prjpath!\%FILENAME_CUSTOM%" 1>nul
+					if ERRORLEVEL 1 (
+						echo ERROR copy "%FILENAME_CUSTOM%" to "!prj!" failed
+						exit /b 8
+					)	
+					echo copy "%FILENAME_CUSTOM%" to "!prj!" OK
+				)
+			)
 		)
 	)
 )
 
+echo remove "%DOWNLOAD_DIR%" ...
 rd /s /q "%DOWNLOAD_DIR%" 1>nul
 if ERRORLEVEL 1 (
-	echo ERROR remove "%DOWNLOAD_DIR%" failed
-	exit /b 7
+	echo FAILURE
+	exit /b 9
 )
+echo OK
 
 explorer "%DOWNLOAD_ROOT%"
 
