4084d2374312cf313d3387edd4f68595a927a910
Tomislav Matas <tomislav.matas@deutschebahn.com>
2024-06-24 22:37:19 +0200

    webdriver/126 Version "1.126.1" Edit "bat/signReleaseMsi.cmd" Add signage of exe file.

--- bat/signReleaseMsi.cmd
+++ bat/signReleaseMsi.cmd
@@ -5,6 +5,8 @@
 :: -- -->< https://learn.microsoft.com/de-de/windows/msix/package/create-certificate-package-signing >
 :: -- 
 :: -- Revision History
+:: -- 2024/06/24:TomislavMatas: 
+:: -- * Add signage of exe file.
 :: -- 2024/04/10:TomislavMatas: 
 :: -- * Initial version
 :: --
@@ -14,8 +16,9 @@ setlocal
 
 set "PROJECT_ROOT=%~dp0.."
 set "SIGNTOOL_EXE=C:\Program Files (x86)\Windows Kits\10\App Certification Kit\signtool.exe"
-set "MSI_FILE_PATH=%PROJECT_ROOT%\src\EZAsesAutoTypeSetup\bin\Release"
-set "PFX_FILE_PATH=%PROJECT_ROOT%\cert\MatasConsultingSelfSigned.pfx"
+set "EXE_FILE=%PROJECT_ROOT%\src\EZAsesAutoType\bin\Release\net8.0-windows\EZAsesAutoType.exe"
+set "PFX_FILE=%PROJECT_ROOT%\cert\MatasConsultingSelfSigned.pfx"
+set "MSI_PATH=%PROJECT_ROOT%\src\EZAsesAutoTypeSetup\bin\Release"
 set /P "PFX_FILE_PASSWORD=pfx file password : "
 
 if not exist "%SIGNTOOL_EXE%" (
@@ -23,13 +26,18 @@ if not exist "%SIGNTOOL_EXE%" (
   goto failure
 )
 
-if not exist "%MSI_FILE_PATH%" (
-  echo "%MSI_FILE_PATH%" DOES NOT EXIST
+if not exist "%EXE_FILE%" (
+  echo "%EXE_FILE%" DOES NOT EXIST
   goto failure
 )
 
-if not exist "%PFX_FILE_PATH%" (
-  echo "%PFX_FILE_PATH%" DOES NOT EXIST
+if not exist "%MSI_PATH%" (
+  echo "%MSI_PATH%" DOES NOT EXIST
+  goto failure
+)
+
+if not exist "%PFX_FILE%" (
+  echo "%PFX_FILE%" DOES NOT EXIST
   goto failure
 )
 
@@ -39,7 +47,14 @@ if "%PFX_FILE_PASSWORD%" == "" (
 )
 
 @echo on
-"%SIGNTOOL_EXE%" sign /f "%PFX_FILE_PATH%" /p %PFX_FILE_PASSWORD% /fd SHA256 "%MSI_FILE_PATH%\*.msi"
+"%SIGNTOOL_EXE%" sign /f "%PFX_FILE%" /p %PFX_FILE_PASSWORD% /fd SHA256 "%EXE_FILE%"
+@if errorlevel 1 @(
+  @echo code signage failed
+  @goto failure
+)
+@echo OK
+
+"%SIGNTOOL_EXE%" sign /f "%PFX_FILE%" /p %PFX_FILE_PASSWORD% /fd SHA256 "%MSI_PATH%\*.msi"
 @if errorlevel 1 @(
   @echo code signage failed
   @goto failure
