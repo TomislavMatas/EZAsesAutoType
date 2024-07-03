98c7fb0d709118d5838851dffca1db97c60bbb10
Tomislav Matas <tomislav.matas@deutschebahn.com>
2024-07-03 20:26:20 +0200

    webdriver/126 Version "1.126.2" Add new UserSettings "ASESDoLogin=true" and "ASESDoPunch=true".

--- src/EZAsesAutoType/Properties/Settings.Designer.cs
+++ src/EZAsesAutoType/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace EZAsesAutoType.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.9.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.10.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -211,6 +211,30 @@ namespace EZAsesAutoType.Properties {
             }
             set {
                 this["ASESPunchOutPM"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        public bool ASESDoLogin {
+            get {
+                return ((bool)(this["ASESDoLogin"]));
+            }
+            set {
+                this["ASESDoLogin"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        public bool ASESDoPunch {
+            get {
+                return ((bool)(this["ASESDoPunch"]));
+            }
+            set {
+                this["ASESDoPunch"] = value;
             }
         }
     }
